5aa5798312bfe575d62c0aac01feb1e73d72e8da
Maria <mariabucursabau@gmail.com>
2022-04-19 12:03:03 +0300

    Stripe

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,23 +1,24 @@
+export const formatNumber = (number) => {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "RON"
+  }).format(number);
+};
 
-export const formatNumber = number => {
-    return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'RON' }).format(number);
-}
-
-
-const API = 'https://stripe-backend-zknsth5hha-uc.a.run.app';
+const API = "https://shop32132131.herokuapp.com";
 
 /**
  * A helper function to fetch data from your API.
  */
 export async function fetchFromAPI(endpointURL, opts) {
-  const { method, body } = { method: 'POST', body: null, ...opts };
+  const { method, body } = { method: "POST", body: null, ...opts };
 
   const res = await fetch(`${API}/${endpointURL}`, {
     method,
     ...(body && { body: JSON.stringify(body) }),
     headers: {
-      'Content-Type': 'application/json',
-    },
+      "Content-Type": "application/json"
+    }
   });
 
   return res.json();
